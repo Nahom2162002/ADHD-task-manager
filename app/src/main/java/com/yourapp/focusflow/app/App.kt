de26107471b2985105fa2e9dc76b814c34e32db0
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-07-11 15:33:34 -0400

    Updated application class to work with WorkManager

--- app/src/main/java/com/yourapp/focusflow/app/App.kt
+++ app/src/main/java/com/yourapp/focusflow/app/App.kt
@@ -1,7 +1,15 @@
 package com.yourapp.focusflow.app
 
 import android.app.Application
+import androidx.privacysandbox.tools.core.generator.build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FocusFlowApp : Application()
+class FocusFlowApp : Application(), androidx.work.Configuration.Provider {
+    @Inject lateinit var workerFactory: androidx.hilt.work.HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        androidx.work.Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
